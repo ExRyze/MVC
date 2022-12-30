1a91a43cbdf12229d2ea053cb15c510590165cfd
ExRyze <vaisyagovinandas@gmail.com>
2022-12-30 17:31:08 +0800

    Init

--- app/controllers/Home.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Home extends Controller {
-  public function index() {
-    $data["title"] = "Home";
-    $this->view("template/header", $data);
-    $this->view("home/index", $data);
-    $this->view("template/header");
-  }
-}
--- app/views/home/index.php
+++
@@ -1,5 +0,0 @@
-<body>
-  <?php $this->view("template/navbar", $data); ?>
-  <div>
-    <h1 class="mb-3"><?=$data["title"]?></h1>
-  </div>
