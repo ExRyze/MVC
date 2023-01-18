a98e44f31685df2c1d9530d9e18a3230fcda5ed0
ExRyze <vaisyagovinandas@gmail.com>
2023-01-18 21:37:26 +0800

    Update - Minor Fixing

--- app/core/Database.php
+++ app/core/Database.php
@@ -31,7 +31,7 @@ class Database {
   }
 
   public function bind($name, $value, $type = null) {
-    if($type === null){
+    if(is_null($type)){
       switch (true) {
         case is_int($value):
           $type = PDO::PARAM_INT;
