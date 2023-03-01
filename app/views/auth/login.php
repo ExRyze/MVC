f6838a24819434626f5986a840dcd5faa07e7ef6
ExRyze <vaisyagovinandas@gmail.com>
2023-03-01 08:16:50 +0800

    Update - Login imgae

--- app/views/auth/login.php
+++ app/views/auth/login.php
@@ -35,7 +35,9 @@
                     <div class="card-body p-0">
                         <!-- Nested Row within Card Body -->
                         <div class="row">
-                            <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+                            <div class="col-lg-6 d-none d-lg-flex justify-content-center align-items-center">
+                                <img class="img-fluid px-3 px-sm-4 mt-3 mb-4" style="width: 25rem;" src="img/undraw_posting_photo.svg" alt="...">
+                            </div>
                             <div class="col-lg-6">
                                 <div class="p-5">
                                     <div class="text-center">
